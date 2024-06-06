6aaf852a5cb1d6ae88d5ed84b7ba7fdac894e40d
henrikvtcodes <commits@henrikvt.com>
2024-06-06 13:57:24 -0400

    bump versions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,73 +1,75 @@
-import { themes } from 'prism-react-renderer';
+import { themes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+const config = {
+  title: "VERSO Open Resource Library",
+  tagline: "Learn, explore, and create open source practices.",
+  favicon:
+    "https://verso.w3.uvm.edu/wp-content/uploads/2023/08/cropped-Logo_verso2-1-1-192x192.png",
 
-export default {
-  title: 'VERSO Open Resource Library',
-  tagline: 'Learn, explore, and create open source practices.',
-  favicon: 'https://verso.w3.uvm.edu/wp-content/uploads/2023/08/cropped-Logo_verso2-1-1-192x192.png',
-
-  url: 'https://openresourcelibrary.com', // TODO: Domain?
-  baseUrl: '/',
+  url: "https://openresourcelibrary.com", // TODO: Domain?
+  baseUrl: "/",
 
   // GitHub pages deployment config.
-  organizationName: 'VERSO-UVM',
-  projectName: 'Open-Resource-Library',
+  organizationName: "VERSO-UVM",
+  projectName: "Open-Resource-Library",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Required for language metadata, not actually internationalized
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en']
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/VERSO-UVM/Open-Resource-Library',
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
+          editUrl: "https://github.com/VERSO-UVM/Open-Resource-Library",
         },
         blog: false,
         pages: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
-      }
+      },
     ],
   ],
 
   themeConfig: {
     colorMode: {
-      respectPrefersColorScheme: true
+      respectPrefersColorScheme: true,
     },
 
     navbar: {
-      title: 'Open Resource Library',
+      title: "Open Resource Library",
       logo: {
-        alt: 'VERSO Logo',
-        src: 'https://verso.w3.uvm.edu/wp-content/uploads/2023/08/cropped-Logo_verso2-1-1-192x192.png'
+        alt: "VERSO Logo",
+        src: "https://verso.w3.uvm.edu/wp-content/uploads/2023/08/cropped-Logo_verso2-1-1-192x192.png",
       },
       items: [
         {
-          href: 'https://verso.w3.uvm.edu',
-          label: 'VERSO Website',
-          position: 'right',
+          href: "https://verso.w3.uvm.edu",
+          label: "VERSO Website",
+          position: "right",
         },
         {
-          href: 'https://github.com/VERSO-UVM/Open-Resource-Library',
-          label: 'GitHub',
-          position: 'right',
-        }
-      ]
+          href: "https://github.com/VERSO-UVM/Open-Resource-Library",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
     },
 
     prism: {
       theme: themes.github,
-      darkTheme: themes.dracula
-    }
-  }
-};
+      darkTheme: themes.dracula,
+    },
+  },
+} satisfies Config;
+
+export default config;
